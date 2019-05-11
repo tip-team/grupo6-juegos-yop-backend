bc4f972b7242c99b01fe8324d34c2c065a1089a5
marinoalan <marinoalanunq@gmail.com>
2019-05-11 13:51:06 -0300

    Se agrego la fecha que llego la notificacion de compra.

--- src/main/java/ar/edu/unq/tip/grupo6/app/model/Pago.java
+++ src/main/java/ar/edu/unq/tip/grupo6/app/model/Pago.java
@@ -1,15 +1,15 @@
 package ar.edu.unq.tip.grupo6.app.model;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@AllArgsConstructor
 @NoArgsConstructor
 @Getter @Setter
 @Entity
@@ -21,5 +21,17 @@ public class Pago {
 	Float monto;
 	Float montoRecibido;
 	String estadoDePago;
+	String fecha;
+	
+	public Pago(String idCompra, String nombreProducto, Float monto, Float montoRecibido, String estadoDePago) {
+		this.idCompra = idCompra;
+		this.nombreProducto = nombreProducto;
+		this.monto = monto;
+		this.montoRecibido = montoRecibido;
+		this.estadoDePago = estadoDePago;
+		LocalDateTime now = LocalDateTime.now();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+		this.fecha = now.format(formatter);
+	}
 	
 }
