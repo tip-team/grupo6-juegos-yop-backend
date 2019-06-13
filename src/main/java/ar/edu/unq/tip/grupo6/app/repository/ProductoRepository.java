3f092667c27ca962367efefe6ce46af432421bda
marinoalan <marinoalanunq@gmail.com>
2019-06-13 00:44:39 -0300

    Se agrego modifying.

--- src/main/java/ar/edu/unq/tip/grupo6/app/repository/ProductoRepository.java
+++ src/main/java/ar/edu/unq/tip/grupo6/app/repository/ProductoRepository.java
@@ -3,6 +3,7 @@ package ar.edu.unq.tip.grupo6.app.repository;
 import java.util.List;
 import javax.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -14,6 +15,7 @@ public interface ProductoRepository extends JpaRepository<Producto, Integer> {
 	@Transactional
 	List<Producto> findAllByOrderByPrioridadAsc();
 	
+	@Modifying
 	@Query("UPDATE Producto producto SET producto.prioridad = :prioridad WHERE producto.id = :id")
 	void updatePrioridad(@Param("id") Integer id, @Param("prioridad") Integer prioridad);
 
